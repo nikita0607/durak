f2c151fe68665f53597b839a9695782491eb121f
nikita0607 <ecfed203@gmail.com>
2022-01-09 23:50:22 +0800

    Add some features

--- main.py
+++ main.py
@@ -17,8 +17,57 @@ class Consts:
     ENTER_X_SHIFT, ENTER_Y_SHIFT = 40, 155
     FIRST_USER_SHIFT_X, FIRST_USER_SHIFT_Y = 50, 245
 
-    TO_STICKER_SHIFT_X, TO_STICKER_SHIFT_Y = 790, 207
+    TO_STICKER_SHIFT_X, TO_STICKER_SHIFT_Y = 395, 120
     STICKER_SHIFT_X, STICKER_SHIFT_Y = 105, 120
+
+
+class Image:
+
+    # Функция вычисления хэша
+    @classmethod
+    def calc_image_hash(cls, image) -> str:
+        resized = cv2.resize(image, (8, 8), interpolation=cv2.INTER_AREA)  # Уменьшим картинку
+        gray_image = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)  # Переведем в черно-белый формат
+        avg = gray_image.mean()  # Среднее значение пикселя
+        ret, threshold_image = cv2.threshold(gray_image, avg, 255, 0)  # Бинаризация по порогу
+
+        # Рассчитаем хэш
+        _hash = ""
+        for x in range(8):
+            for y in range(8):
+                val = threshold_image[x, y]
+                if val == 255:
+                    _hash = _hash + "1"
+                else:
+                    _hash = _hash + "0"
+
+        return _hash
+
+    @staticmethod
+    def compare_image_hash(hash1, hash2) -> int:
+        l = len(hash1)
+        i = 0
+        count = 0
+        while i < l:
+            if hash1[i] != hash2[i]:
+                count = count + 1
+            i = i + 1
+        return count
+
+    @classmethod
+    def compare_images(cls, img1, img2) -> int:
+        hash_1 = cls.calc_image_hash(img1)
+        hash_2 = cls.calc_image_hash(img2)
+
+        return cls.compare_image_hash(hash_1, hash_2)
+
+    @classmethod
+    def take_black(cls, src):
+        size = src.shape[1], src.shape[0]
+        gray_img = cv2.imread("light_gray.png")
+        gray_img = cv2.resize(gray_img, size)
+
+        return cv2.add(src, gray_img)
 
 
 class Worker:
@@ -26,35 +75,39 @@ class Worker:
     runed = True
 
     def __init__(self):
-        self.root = Tk()
-        self.root.resizable(False, False)
-        self.root.wm_geometry(Consts.WIN_SIZE)
+        self.first_sticker_compare_num = 0
+
+    def run_config(self):
+        root = Tk()
+
+        def set_first_sticker():
+            shot = self.scnreenshot_shift(self.pos_x + Consts.TO_STICKER_SHIFT_X,
+                                          self.pos_y + Consts.TO_STICKER_SHIFT_Y,
+                                          Consts.STICKER_SHIFT_X, Consts.STICKER_SHIFT_Y)
+            black = Image.take_black(shot)[95:, :]
+
+            self.first_sticker_compare_num = Image.compare_images(black, cv2.imread("one_sticker.png"))
+            print(f"Set: {self.first_sticker_compare_num}")
+
+        bt1 = Button(text="First sticker with 1", command=set_first_sticker)
+        bt1.pack()
+
+        bt_start = Button(text="Ok, i'm ready", command=lambda: (root.destroy(), self._run()))
+        bt_start.pack()
+
+        root.mainloop()
 
     def scnreenshot_shift(self, x1, y1, x_shift, y_shift):
         return self.screenshot(x1, y1, x1+x_shift, y1+y_shift)
 
-    def screenshot(self, x1, y1, x2, y2):
-        shot = pg.screenshot("test_scr.png")
-        shot = cv2.imread("test_scr.png")[Consts.TO_STICKER_SHIFT_Y:Consts.TO_STICKER_SHIFT_Y + Consts.STICKER_SHIFT_Y,
-               Consts.TO_STICKER_SHIFT_X:Consts.TO_STICKER_SHIFT_X + Consts.STICKER_SHIFT_X]
+    @staticmethod
+    def screenshot(x1, y1, x2, y2):
+        pg.screenshot("test_scr.png")
+        shot = cv2.imread("test_scr.png")[y1:y2, x1:x2]
 
         return shot
 
-    def take_black(self, src):
-        size = src.shape[1], src.shape[0]
-        gray_img = cv2.imread("test2.png")
-        gray_img = cv2.resize(gray_img, size)
-
-        img = cv2.add(src, gray_img)[95:, :]
-
-        cv2.imwrite("test.png", img)
-
-        cv2.imshow("test", img)
-        cv2.waitKey(0)
-
-        return img
-
-    def enter(self, last: Iterable, new: str):
+    def enter_name(self, last: Iterable, new: str):
         pg.moveTo(self.pos_x + Consts.ENTER_X_SHIFT, self.pos_y + Consts.ENTER_Y_SHIFT)
         pg.click()
         pg.click()
@@ -91,33 +144,47 @@ class Worker:
         self.runed = False
 
     def run(self):
+        root = Tk()
+        root.resizable(False, False)
+        root.wm_geometry(Consts.WIN_SIZE)
 
         kb.add_hotkey("ctrl+s", worker.stop)
-        Button(text='start', command=self._run).pack()
-        self.root.mainloop()
+
+        def start():
+            self.pos_x, self.pos_y = root.winfo_x(), root.winfo_y()
+            root.destroy()
+            self.run_config()
+
+        Button(text='start', command=start).pack()
+        root.mainloop()
+
+    def delay_loop(self, time_ms: int):
+        while self.runed and time_ms > 0:
+            time_ms -= 1
+            sleep(0.001)
 
     def _run(self):
         name = [0]
         last_name = ""
-        self.pos_x, self.pos_y = self.root.winfo_x(), self.root.winfo_y()
-        self.root.destroy()
+
         print(self.pos_x, self.pos_y)
 
         while self.runed:
-            self.enter(last_name, self.name_to_str(name))
+            self.enter_name(last_name, self.name_to_str(name))
             last_name, name = name, self.get_new_name(name)
 
             pg.moveTo(self.pos_x + Consts.FIRST_USER_SHIFT_X, self.pos_y + Consts.FIRST_USER_SHIFT_Y)
-            sleep(2)
+            self.delay_loop(2000)
             pg.click()
-            sleep(5)
+            self.delay_loop(5000)
 
             shot = self.scnreenshot_shift(self.pos_x+Consts.TO_STICKER_SHIFT_X, self.pos_y+Consts.TO_STICKER_SHIFT_Y,
                                           Consts.STICKER_SHIFT_X, Consts.STICKER_SHIFT_Y)
-            cv2.imshow("test", self.take_black(shot))
-            cv2.waitKey(0)
+            black = Image.take_black(shot)[95:, :]
 
-            sleep(5)
+            print("Comparing: ", Image.compare_images(black, cv2.imread("one_sticker.png")) == self.first_sticker_compare_num)
+
+            self.delay_loop(5000)
 
 
 worker = Worker()
